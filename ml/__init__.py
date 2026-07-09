e72304894bb4c8eb33b46db1569e80ced1a00473
Juan villada <villadaurechejuandavid20@gmail.com>
2026-07-09 08:59:11 -0500

    Import torch opcional en ml/__init__.py (bot web funciona sin torch)

--- ml/__init__.py
+++ ml/__init__.py
@@ -1,4 +1,12 @@
 # Machine Learning module
+# Imports de torch son opcionales para que el bot web funcione sin torch instalado
 from .features import FeatureGenerator
 from .train import ModelTrainer
-from .neural_brain import NeuralTradingBrain, NeuralTrainer, train_from_backtest, extract_features
+
+try:
+    from .neural_brain import NeuralTradingBrain, NeuralTrainer, train_from_backtest, extract_features
+except (ImportError, ModuleNotFoundError):
+    NeuralTradingBrain = None
+    NeuralTrainer = None
+    train_from_backtest = None
+    extract_features = None
